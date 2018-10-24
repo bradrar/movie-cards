adc772b1be98c4bee83f367bd6c5f8673e5159f6
Daniel Beaupre <danbopes@gmail.com>
2018-10-24 10:28:04 -0400

    Changes to allow for dynamic images.

--- src/Movies/MovieCard.js
+++ src/Movies/MovieCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+const movieImages = require.context("../images/", true, /\.jpg$/);
+
 const MovieCard = (props) => {
     return (      
         <div className="d-flex mb-5">
 
           
         <div className="card" style={{width: 18 + 'rem'}}>
-        <img className="card-img-top"  src={require("../images/Ghost_In_The_Shell_2_0.jpg")} alt="" />
+        <img className="card-img-top" src={movieImages(props.movie.imageUrl)} alt="" />
         <div className="card-body">
           <h5 className="card-title">{props.movie.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{props.movie.subtitle}</h6>
--- src/Router/Reducers/MovieList-reducer.js
+++ src/Router/Reducers/MovieList-reducer.js
@@ -9,7 +9,7 @@ const reducer = () => {
                 "subtitle": "Final Fantasy XV",
                 "description": "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
                 "year": 2016,
-                "imageUrl": "../images/Kingsglaive_Final_Fantasy_XV.jpg",
+                "imageUrl": "./Kingsglaive_Final_Fantasy_XV.jpg",
                 "rating": 4.5
             },
             {
@@ -18,7 +18,7 @@ const reducer = () => {
                 "subtitle": "Spirits Within",
                 "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
                 "year": 2001,
-                "imageUrl": "../images/Final_Fantasy_Spirits_Within.jpg",
+                "imageUrl": "./Final_Fantasy_Spirits_Within.jpg",
                 "rating": 4.5
             },
             {
@@ -27,7 +27,7 @@ const reducer = () => {
                 "subtitle": "Ghost In The Shell",
                 "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
                 "year": 2008,
-                "imageUrl": "../images/Ghost_In_The_Shell_2_0.jpg",
+                "imageUrl": "./Ghost_In_The_Shell_2_0.jpg",
                 "rating": 5
             },
             {
@@ -36,7 +36,7 @@ const reducer = () => {
                 "subtitle": "Appleseed Alpha",
                 "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
                 "year": 2014,
-                "imageUrl": "../images/Appleseed_Alpha.jpg",
+                "imageUrl": "./Appleseed_Alpha.jpg",
                 "rating": 3.8
             },
             {
@@ -45,7 +45,7 @@ const reducer = () => {
                 "subtitle": "Vendetta",
                 "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
                 "year": 2014,
-                "imageUrl": "../images/Resident_Evil_Vendetta.jpg",
+                "imageUrl": "./Resident_Evil_Vendetta.jpg",
                 "rating": 4.2
             }
         ]
